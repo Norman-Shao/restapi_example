8fefc372823b26bf379c6abf95f695f1dd56274e
gevin <flyhigher139@gmail.com>
2017-06-14 21:42:03 +0800

    chapter2

--- app.py
+++ app.py
@@ -35,16 +35,26 @@ def hello_rest():
     </html>
     '''
 
-class PostListView(MethodView):
+class PostListCreateView(MethodView):
     def get(self):
         posts = Post.objects.all()
+        category = request.args.get('category')
+        tag = request.args.get('tag')
+
+        if category:
+            posts = posts.filter(category=category)
+
+        if tag:
+            posts = posts.filter(tag=tag)
+
         data = [post.to_dict() for post in posts]
 
         return jsonify(posts=data)
 
     def post(self):
         '''
-        post data example:
+        Send a json data as follow will create a new blog instance:
+
         {
             "title": "Title 1",
             "slug": "title-1",
@@ -55,11 +65,10 @@ class PostListView(MethodView):
             "tags": ["tag1", "tag2"]
         }
         '''
+
         data = request.get_json()
+
         article = Post()
-
-        # return jsonify(data)
-
         article.title = data.get('title')
         article.slug = data.get('slug')
         article.abstract = data.get('abstract')
@@ -72,9 +81,90 @@ class PostListView(MethodView):
 
         return jsonify(article.to_dict())
 
-app.add_url_rule('/posts/', view_func=PostListView.as_view('posts'))
 
 
+class PostDetailGetUpdateDeleteView(MethodView):
+    def get(self, slug):
+        try:
+            post = Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return jsonify({'error': 'post does not exist'}), 404
+
+        return jsonify(post=post.to_dict())
+
+    def put(self, slug):
+        try:
+            post = Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return jsonify({'error': 'post does not exist'}), 404
+
+        data = request.get_json()
+
+        if not data.get('title'):
+            return 'title is needed in request data', 400
+
+        if not data.get('slug'):
+            return 'slug is needed in request data', 400
+
+        if not data.get('abstract'):
+            return 'abstract is needed in request data', 400
+
+        if not data.get('raw'):
+            return 'raw is needed in request data', 400
+
+        if not data.get('author'):
+            return 'author is needed in request data', 400
+
+        if not data.get('category'):
+            return 'category is needed in request data', 400
+
+        if not data.get('tags'):
+            return 'tags is needed in request data', 400
+
+        
+
+        post.title = data['title']
+        post.slug = data['slug']
+        post.abstract = data['abstract']
+        post.raw = data['raw']
+        post.author = data['author']
+        post.category = data['category']
+        post.tags = data['tags']
+
+        post.save()
+
+        return jsonify(post=post.to_dict())
+
+    def patch(self, slug):
+        try:
+            post = Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return jsonify({'error': 'post does not exist'}), 404
+
+        data = request.get_json()
+
+        post.title = data.get('title') or post.title 
+        post.slug = data.get('slug') or post.slug
+        post.abstract = data.get('abstract') or post.abstract
+        post.raw = data.get('raw') or post.raw
+        post.author = data.get('author') or post.author
+        post.category = data.get('category') or post.category
+        post.tags = data.get('tags') or post.tags
+
+        return jsonify(post=post.to_dict())
+
+    def delete(self, slug):
+        try:
+            post = Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return jsonify({'error': 'post does not exist'}), 404
+
+        post.delete()
+
+        return 'Succeed to delete post', 204
+
+app.add_url_rule('/posts/', view_func=PostListCreateView.as_view('posts'))
+app.add_url_rule('/posts/<slug>/', view_func=PostDetailGetUpdateDeleteView.as_view('post'))
 
 if __name__ == '__main__':
     app.run(
